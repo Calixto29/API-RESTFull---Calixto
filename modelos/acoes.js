8e60ebb18e80e5c70454e3a374ab7bb84174bf5e
Fernando <fernando.junior2906@gmail.coom>
2022-05-01 09:39:22 -0300

    install lib moment e correção da conversão da data

--- modelos/acoes.js
+++ modelos/acoes.js
@@ -1,10 +1,14 @@
-const conexao = require('../conexaoBd/conexao')
+const moment = require('moment');
+const conexao = require('../conexaoBd/conexao');
 
 class Cadastrar { //Cadastrar
     cria(cadastro) { //Cadastra o usuário
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS') 
+        const data = moment(cadastro.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS') //converte o formato de data 
+        const cadastroDataCriacao = {...cadastro, dataCriacao, data}
         const sql = 'INSERT INTO teste SET ?' // INSERT INTO NOME_TABELA SET ?
                             //OBJETO        //FUNÇAO
-        conexao.query(sql, cadastro, (erro, resultados) => {
+        conexao.query(sql, cadastroDataCriacao, (erro, resultados) => {
             if (erro) {
                 console.log(erro)
             } else {
