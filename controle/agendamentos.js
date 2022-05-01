98f1cdecb44e8d298c116c0ff40159bb1f87ef4a
Fernando <fernando.junior2906@gmail.coom>
2022-05-01 16:54:22 -0300

    pesquisa de usuario

--- controle/agendamentos.js
+++ controle/agendamentos.js
@@ -1,14 +1,23 @@
 const Cadastro = require('../modelos/acoes')
 
 module.exports = app => {
-    app.get('/api', (req, res) => res.send('Você está na rota da API de agendamento de tarefas!')); 
+    app.get('/api', (req, res) => {
+        Cadastro.listarUser(res)
+    }); 
+
+    app.get('/api/:id', (req, res)=> {
+        const id = parseInt(req.params.id)
+
+        Cadastro.pesquisarIdUser(id, res)
+        
+    })
 
     app.post('/api', (req, res) => {
         const cadastro = req.body
 
-        Cadastro.cria(cadastro, res) //erro
+        Cadastro.criaUser(cadastro, res) 
         
-        //res.send('Post de Agendamento!')
+        
     });
 }
 
--- modelos/acoes.js
+++ modelos/acoes.js
@@ -1,11 +1,35 @@
 const moment = require('moment');
 const conexao = require('../conexaoBd/conexao');
 
-class Cadastrar { //Cadastrar
-    cria(cadastro, res) { //Cadastra o usuário
+class Cadastrar { //Cadastrar User
+    criaUser(cadastro, res) { //Cadastra o usuário
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS') 
         const data = moment(cadastro.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS') //converte o formato de data 
-        const cadastroDataCriacao = {...cadastro, dataCriacao, data}
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const nameValido = cadastro.teste2.length >=5
+
+        const validarDados = [
+            {
+                nome: 'data', 
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+                
+            },
+            {
+                nome: 'user',
+                valido: nameValido,
+                mensagem: 'Nome deve ter pelo menos cinco caracteres'
+
+            }
+        ]
+
+        const erros = validarDados.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)            
+        } else {
+            const cadastroDataCriacao = {...cadastro, dataCriacao, data}
         const sql = 'INSERT INTO teste SET ?' // INSERT INTO NOME_TABELA SET ?
                             //OBJETO        //FUNÇAO
         conexao.query(sql, cadastroDataCriacao, (erro, resultados) => {
@@ -15,7 +39,36 @@ class Cadastrar { //Cadastrar
                 res.status(201).json(resultados)
             }
         })
+        }
+
         
+        
+    }
+
+    listarUser(res) {
+        const sql = 'SELECT * FROM teste' //teste nome_tabela
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    pesquisarIdUser(id, res) {
+        const sql = `SELECT * FROM teste WHERE id=${id}` 
+
+        conexao.query(sql, (erro, resultados) => {
+            const dadosUser = resultados[0]
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(201).json(dadosUser)
+
+            }
+        })
     }
 }
 
